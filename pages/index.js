93b962b9d71c4d927d7a3e224649cf88907c40c8
Attinder <Attinderkahlon@gmail.com>
2022-10-30 04:58:32 +1100

    added cards

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,18 @@
 import Head from 'next/head'
 import { BsFillMoonStarsFill } from 'react-icons/bs'
+import { AiFillGithub, AiFillLinkedin } from 'react-icons/ai'
+import Image from 'next/image'
+import deved from '../public/dev-ed-wave.png'
+import code from '../public/code.png'
+import design from '../public/design.png'
+import consulting from '../public/consulting.png'
+import web1 from '../public/web1.png'
+import web2 from '../public/web2.png'
+import web3 from '../public/web3.png'
+import web4 from '../public/web4.png'
+import web5 from '../public/web5.png'
+import web6 from '../public/web6.png'
+
 export default function Home() {
   return (
     <div>
@@ -12,7 +25,7 @@ export default function Home() {
       <main className=' bg-white px-10'>
         <section className=' min-h-screen'>
           <nav className='py-10 mb-12 flex justify-between'>
-            <h1 className='text-xl'>Developed by Attinder</h1>
+            <h1 className='text-xl font-burtons'>Developed by Attinder</h1>
             <ul className='flex items-center'>
               <li>
                 <BsFillMoonStarsFill className='cursor-ponter text-2xl' />
@@ -27,6 +40,148 @@ export default function Home() {
               </li>
             </ul>
           </nav>
+          <div className='text-center p-10'>
+            <h2 className='text-5xl py-2 text-teal-600 font-medium'>
+              Attinder Singh
+            </h2>
+            <h3 className='text-2xl py-2'>Front-End Developer</h3>
+            <p className='text-md py-5 leading-8 text-gray-800'>
+              My name is Attinder and I'm a Front End Developer from Australia
+              This is my digital garden, where I write about the things I'm
+              working on and share what I've learned. 😊
+            </p>
+
+            <div className='text-5xl flex justify-center gap-16 py-3 text-gray-600'>
+              <AiFillLinkedin />
+              <AiFillGithub />
+            </div>
+            <div className='mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 relative overflow-hidden mt-20 md:h-96 md:w-96'>
+              <Image
+                src={deved}
+                layout='fill'
+                alt='profile'
+                objectFit='cover'
+              />
+            </div>
+          </div>
+        </section>
+        <section>
+          <div>
+            <h3 className='text-3xl py-1 dark:text-white '>Services I offer</h3>
+            <p className='text-md py-2 leading-8 text-gray-800 dark:text-gray-200'>
+              Since the beginning of my journey as a freelance designer and
+              developer, I've done remote work for
+              <span className='text-teal-500'> agencies </span>
+              consulted for <span className='text-teal-500'>startups </span>
+              and collaborated with talanted people to create digital products
+              for both business and consumer use.
+            </p>
+          </div>
+          <div className='lg:flex gap-10'>
+            <div className='text-center shadow-lg p-10 rounded-xl my-10  dark:bg-white flex-1'>
+              <Image src={design} alt='design' width={100} height={100} />
+              <h3 className='text-lg font-medium pt-8 pb-2  '>
+                Beautiful Designs
+              </h3>
+              <p className='py-2'>
+                Creating elegant designs suited for your needs following core
+                design theory.
+              </p>
+              <h4 className='py-4 text-teal-600'>Design Tools I Use</h4>
+              <p className='text-gray-800 py-1'>Photoshop</p>
+              <p className='text-gray-800 py-1'>Illustrator</p>
+              <p className='text-gray-800 py-1'>Figma</p>
+            </div>
+            <div className='text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white flex-1'>
+              <Image src={code} alt='code' width={100} height={100} />
+              <h3 className='text-lg font-medium pt-8 pb-2 '>
+                Code your dream project
+              </h3>
+              <p className='py-2'>
+                Do you have an idea for your next great website? Let's make it a
+                reality.
+              </p>
+              <h4 className='py-4 text-teal-600'>Design Tools I Use</h4>
+              <p className='text-gray-800 py-1'>Photoshop</p>
+              <p className='text-gray-800 py-1'>Illustrator</p>
+              <p className='text-gray-800 py-1'>Figma</p>
+            </div>
+            <div className='text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white flex-1'>
+              <Image
+                src={consulting}
+                alt='consulting'
+                width={100}
+                height={100}
+              />
+              <h3 className='text-lg font-medium pt-8 pb-2 '>Consulting</h3>
+              <p className='py-2'>
+                Are you interested in feedback for your current project? I can
+                give you tips and tricks to level it up.
+              </p>
+              <h4 className='py-4 text-teal-600'>Design Tools I Use</h4>
+              <p className='text-gray-800 py-1'>Photoshop</p>
+              <p className='text-gray-800 py-1'>Illustrator</p>
+              <p className='text-gray-800 py-1'>Figma</p>
+            </div>
+          </div>
+        </section>
+        <section className='py-10'>
+          <div>
+            <h3 className='text-3xl py-1 dark:text-white '>Portofolio</h3>
+          </div>
+          <div className='flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap'>
+            <div className='basis-1/3 flex-1 '>
+              <Image
+                className='rounded-lg object-cover'
+                width={'100%'}
+                height={'100%'}
+                layout='responsive'
+                src={web1}
+                alt='web1'
+              />
+            </div>
+
+            <div className='basis-1/3 flex-1'>
+              <Image
+                className='rounded-lg object-cover'
+                width={'100%'}
+                height={'100%'}
+                layout='responsive'
+                src={web3}
+                alt='web3'
+              />
+            </div>
+            <div className='basis-1/3 flex-1'>
+              <Image
+                className='rounded-lg object-cover'
+                width={'100%'}
+                height={'100%'}
+                layout='responsive'
+                src={web4}
+                alt='web4'
+              />
+            </div>
+            <div className='basis-1/3 flex-1'>
+              <Image
+                className='rounded-lg object-cover'
+                width={'100%'}
+                height={'100%'}
+                layout='responsive'
+                src={web5}
+                alt='web5'
+              />
+            </div>
+            <div className='basis-1/3 flex-1'>
+              <Image
+                className='rounded-lg object-cover'
+                width={'100%'}
+                height={'100%'}
+                layout='responsive'
+                src={web6}
+                alt='web6'
+              />
+            </div>
+          </div>
         </section>
       </main>
     </div>
