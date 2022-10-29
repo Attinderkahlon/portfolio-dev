9beee820174ed70c54d07199c2e5ee5edce7ba6f
Attinder <105198941+Attinderkahlon@users.noreply.github.com>
2022-10-29 15:17:10 +1100

    tailwind package

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,5 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-
+import { BsFillMoonStarsFill } from 'react-icons/bs'
 export default function Home() {
   return (
     <div>
@@ -11,7 +9,26 @@ export default function Home() {
         <link rel='icon' href='/favicon.ico' />
       </Head>
 
-      <main></main>
+      <main className=' bg-white px-10'>
+        <section className=' min-h-screen'>
+          <nav className='py-10 mb-12 flex justify-between'>
+            <h1 className='text-xl'>Developed by Attinder</h1>
+            <ul className='flex items-center'>
+              <li>
+                <BsFillMoonStarsFill className='cursor-ponter text-2xl' />
+              </li>
+              <li>
+                <a
+                  className=' bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8'
+                  href='#'
+                >
+                  Resume
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </section>
+      </main>
     </div>
   )
 }
